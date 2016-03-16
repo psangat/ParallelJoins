839c247188b80c98dafe9dc7adc56418955e43d9
psangat <psangat@hotmail.com>
2016-03-16 20:20:47 +1100

    Proper functions for list and Map

--- src/ParallelJoinSpark.scala
+++ src/ParallelJoinSpark.scala
@@ -1,16 +1,38 @@
 import org.apache.spark.{TaskContext, SparkContext, SparkConf}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 /**
   * Created by PRSANGAT on 3/15/2016.
   */
 object ParallelJoinSpark {
 
+  def getMap() = Map(
+    1 -> "uno",
+    4 -> "test",
+    5 -> "tiger",
+    6 -> "delete")
+
+  def getList() = List(
+    1 -> "one",
+    2 -> "two",
+    3 -> "three",
+    4 -> "four",
+    5 -> "five",
+    6 -> "six",
+    7 -> "seven",
+    8 -> "eight",
+    9 -> "nine",
+    10 -> "ten")
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Parallel Join").setMaster("local[*]")
     val sc = new SparkContext(conf)
+    // Logger.getLogger("org").setLevel(Level.ERROR)
+    // Logger.getLogger("akka").setLevel(Level.ERROR)
 
-    val t2 = sc.parallelize(List(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five", 6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine", 10 -> "ten"), 6)
-    val t1 = Map(1 -> "uno", 4 -> "test", 5 -> "tiger", 6 -> "delete")
+    val t2 = sc.parallelize(getList(), 6)
+    val t1 = getMap()
 
     val bt1 = sc.broadcast(t1)
     t2.foreachPartition(p => {
@@ -19,7 +41,7 @@ object ParallelJoinSpark {
       val taskId = tc.taskAttemptId()
       p.foreach(rec => {
         val filteredValue = bt1.value.filterKeys(_.equals(rec._1))
-        val mappedFilterValue = filteredValue.map { case (k, v) => ("Key: " + k, " Values: " + (v, rec._2), " Task ID: " + taskId, " PartitionId: " + partId) }
+        val mappedFilterValue = filteredValue.map { case (k, v) => ("Key: " + k, " Values: " +(v, rec._2), " Task ID: " + taskId, " PartitionId: " + partId) }
         mappedFilterValue.foreach(println)
       })
     })
